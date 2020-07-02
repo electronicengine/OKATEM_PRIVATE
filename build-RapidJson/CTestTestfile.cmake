8d3f6b068540746d1009a971ae9c394dbe0e9c0c
electronicengine <y.bulbul@hotmail.com>
2020-07-02 13:05:43 +0300

    hello

---
+++ build-RapidJson/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/yusuf/Documents/workspace/rapidjson
+# Build directory: /home/yusuf/Documents/workspace/rapidjson/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("example")
+subdirs("test")
---
+++ build-RapidJson/example/CMakeFiles/sortkeys.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sortkeys.dir/sortkeys/sortkeys.cpp.o"
+  "../bin/sortkeys.pdb"
+  "../bin/sortkeys"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sortkeys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
